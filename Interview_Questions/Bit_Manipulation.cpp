5f295477a802b427130771cd56c9679d54cde55c
Kumar Ramesh Chakravarthy <69975720+KC-git-usr@users.noreply.github.com>
2023-05-10 19:05:23 -0400

    Added Bit_Manipulation.cpp

---
+++ Interview_Questions/Bit_Manipulation.cpp
@@ -0,0 +1,71 @@
+//
+// Created by Kumar Chakravarthy on 09-May-23.
+//
+
+#include <iostream>
+#include <bitset>
+#include <stdlib.h>
+
+
+char Bit_get(unsigned char* value, unsigned char bit_number) {
+    unsigned char copy_of_value = *value;
+
+    while (bit_number-- > 0)
+        copy_of_value >>= 1;
+
+    return (copy_of_value & 1);
+}
+
+
+char Bit_set(unsigned char* value, unsigned char bit_number) {
+    unsigned char mask = 0b00000001;
+
+    while(bit_number-- > 0)
+        mask <<= 1;
+
+    return (mask | *value);
+}
+
+
+char Bit_clear(unsigned char* value, unsigned char bit_number) {
+    unsigned char mask = 0b00000001;
+
+    while(bit_number-- > 0)
+        mask <<= 1;
+
+    return (mask ^ *value);
+}
+
+
+int main() {
+    unsigned char input_value_1 = 0b00001001;
+    unsigned char input_bit_number_1 = 3;
+    std::bitset<8> x;
+
+    x = Bit_get(&input_value_1, input_bit_number_1);
+    std::cout << "O/P of Bit_get : " << x << std::endl;
+
+    x = Bit_set(&input_value_1, input_bit_number_1);
+    std::cout << "O/P of Bit_set : " << x << std::endl;
+
+    x = Bit_clear(&input_value_1, input_bit_number_1);
+    std::cout << "O/P of Bit_set : " << x << std::endl;
+
+    unsigned char input_value_2 = 0xf;
+    unsigned char input_bit_number_2 = 3;
+
+    x = Bit_get(&input_value_2, input_bit_number_2);
+    std::cout << "O/P of Bit_get : " << x << std::endl;
+
+    x = Bit_set(&input_value_2, input_bit_number_2);
+    std::cout << "O/P of Bit_set : " << x << std::endl;
+
+    x = Bit_clear(&input_value_2, input_bit_number_2);
+    std::cout << "O/P of Bit_set : " << x << std::endl;
+
+    int right;
+    for(int i = 0; i < 10; ++i) {
+        right = rand() % 2;
+        std::cout << "right : " << right << std::endl;
+    }
+}
